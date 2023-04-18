ab5f0a6b95c9e96bb2fbf30bcd83e07db7b6a063
Woochan Park <70905219+Woozzang@users.noreply.github.com>
2023-04-18 21:35:25 +0900

    Create 17298.swift

---
+++ BOJ/17298.swift
@@ -0,0 +1,83 @@
+import Foundation
+
+let fio = FileIO()
+
+var stack = [Int]()
+
+let cnt = fio.readInt()
+
+var arr = Array(repeating: 0, count: cnt)
+
+var ret = Array(repeating: -1, count: cnt)
+
+for i in (0..<cnt) {
+  
+  arr[i] = fio.readInt()
+  
+  while !stack.isEmpty && arr[stack.last!] < arr[i] {
+    ret[stack.removeLast()] = arr[i]
+  }
+  
+  stack.append(i)
+}
+
+print(ret.map{"\($0)"}.joined(separator: " "))
+
+final class FileIO {
+    private let buffer:[UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
